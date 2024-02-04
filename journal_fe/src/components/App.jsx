7f9909c01f40b3dfaeeeab89c62449a0333c2891
mini109 <derdriumcateer@gmail.com>
2024-02-05 09:58:12 +1000

    created components folder

---
+++ journal_fe/src/components/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './Home'
+import NewEntry from './NewEntry'
+import CategorySelection from './CategorySelection'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>My Journal</h1>
+      <Home />
+      <NewEntry/>
+      <CategorySelection/>
+    </>
+  )
+}
+
+export default App
