8bbbd9f594e789a3705a6a2a5095a975ca998f97
mini109 <derdriumcateer@gmail.com>
2024-02-05 11:13:07 +1000

    created intials routes

--- journal_fe/src/components/App.jsx
+++ journal_fe/src/components/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import NewEntry from './NewEntry'
 import CategorySelection from './CategorySelection'
 
 function App() {
-  
+
 
   return (
     <>
       <h1>My Journal</h1>
-      <Home />
-      <NewEntry/>
-      <CategorySelection/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<CategorySelection />} />
+          <Route path="/entry">
+            <Route path="new" element={<NewEntry />} />
+          </Route>
+          <Route path="*" element={<h1>ERROR PAGE NOT FOUND</h1>}/>
+        </Routes>
+      </Router>
+
     </>
   )
 }
