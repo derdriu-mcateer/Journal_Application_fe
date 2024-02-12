e5ebc0a44b4390ca843882c30671d652f18336fb
mini109 <derdriumcateer@gmail.com>
2024-02-13 09:28:37 +1000

    added nav bar styling and title changes

--- journal_fe/src/components/NavBar.jsx
+++ journal_fe/src/components/NavBar.jsx
@@ -16,10 +16,15 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
-        <Link to="/">
-            <h4 className="logo">My Journal</h4>
+        <Link to="/" className="d-flex align-items-center">
+          <h4 className="logo">My Journal</h4>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-journal" viewBox="0 0 16 16">
+            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+          </svg>
         </Link>
-        
+
+
         <button
           className="navbar-toggler"
           type="button"
